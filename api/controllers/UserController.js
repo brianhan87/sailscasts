595e8ecf73187b2c94c364f52c3d93c3bc6ac95e
Brian Han <brianhan87usa@gmail.com>
2013-12-15 19:35:40 -0500

    Add user controller that renders newform

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -17,14 +17,12 @@
 
 module.exports = {
     
-  
-
-
   /**
    * Overrides for the settings in `config/controllers.js`
    * (specific to UserController)
    */
-  _config: {}
 
-  
+  'new': function (req, res) {
+    res.view();
+  }
 };
